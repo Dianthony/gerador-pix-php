11619c421261101ae8a2ae1cd13f1217b54033d9
Dianthony Alves <119366551+Dianthony@users.noreply.github.com>
2024-06-21 16:54:20 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,75 @@
+function typeMask(){
+    let mask = document.querySelector("input[name=keytype]:checked");
+    let key = document.querySelector("#keyTransfer");
+    key.disabled = false;
+    
 
+    if(mask.value == 1){ 
+        key.setAttribute('type','text')
+        //máscara para telefone
+        key.addEventListener("input", () => {
+            // limpar caracteres não numéricos
+            if(key.type === "text"){
+                const cleanField = key.value.replace(/\D/g, "").substring(0,16)
+                        
+                let numberArray = cleanField.split("");
+    
+                let numberFormat = "";
+    
+                if(numberArray.length > 0){
+                    numberFormat += `(${numberArray.slice(0,2).join("")})`
+                }
+                if(numberArray.length > 2){
+                    numberFormat += ` ${numberArray.slice(2,7).join("")}`
+                }
+                if(numberArray.length > 7){
+                    numberFormat += `-${numberArray.slice(7,11).join("")}`
+                }
+                
+                key.value = numberFormat;
+            }
+            else {
+
+            }
+
+        })
+    }
+    else if(mask.value == 2){
+        key.setAttribute('type','email')
+        key.value = ""
+    }
+    else if(mask.value == 3){
+        key.setAttribute('type','text')
+        
+         key.addEventListener("input", func = () => {
+            
+            if(key.type === "text"){
+                // limpar caracteres não numéricos
+                const cleanField = key.value.replace(/\D/g, "").substring(0,14)
+                            
+                let numberArray = cleanField.split("");
+
+                let numberFormat = "";
+
+                if(numberArray.length > 0){
+                    numberFormat += `${numberArray.slice(0,3).join("")}`
+                }
+                if(numberArray.length > 3){
+                    numberFormat += `.${numberArray.slice(3,6).join("")}`
+                }
+                if(numberArray.length > 6){
+                    numberFormat += `.${numberArray.slice(6,9).join("")}`
+                }
+                if(numberArray.length > 9){
+                    numberFormat += `-${numberArray.slice(9,11).join("")}`
+                }
+
+                key.value = numberFormat;
+            }
+            else {}
+         })
+    }
+    else if(mask.value == 4){
+
+    }
+}
