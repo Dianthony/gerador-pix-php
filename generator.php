62f88184d0e5bda301657843f7b34bc360bd6f96
Dianthony Alves <119366551+Dianthony@users.noreply.github.com>
2024-07-16 19:54:37 -0300

    Add files via upload

---
+++ generator.php
@@ -0,0 +1,166 @@
+<?php
+
+    session_start();
+
+    $keyPix = $_POST['keyTransfer'];
+    $namePix = $_POST['nameTransfer'];
+    $cityPix = $_POST['cityTransfer'];
+    $valuePix = $_POST['valueTransfer'];
+    $idPix = $_POST['idTransfer'];
+
+    $typeKey = $_POST['keytype'];
+
+    $remove = array("(", ")", "-", " ", ".");
+    $broken = str_replace($remove, "", $keyPix);
+
+    $removeReal = array("R", "$","\xC2\xA0");
+    $prevalue = str_replace($removeReal, "", $valuePix);
+    
+    $removeValue = array(",");
+    $brokenValue = str_replace($removeValue, ".", $prevalue);
+
+    if($typeKey == 1){
+
+        $newKeyPix = "+55".$broken;
+
+        function formataCampo($id, $valor){
+            return $id . str_pad(strlen($valor), 2, '0', STR_PAD_LEFT) . $valor;
+        }
+    
+        function calculaCRC16($dados){
+            $resultado = 0xFFFF;
+            for ($i = 0; $i < strlen($dados); $i++){
+                $resultado ^= (ord($dados[$i]) << 8);
+                for ($j = 0; $j <8; $j++){
+                    if($resultado & 0x8000){
+                        $resultado = ($resultado << 1) ^ 0x1021;
+                    } 
+                    else{
+                        $resultado <<= 1;
+                    }
+                    $resultado &= 0xFFFF;
+                }
+            }
+            return strtoupper(str_pad(dechex($resultado), 4, '0', STR_PAD_LEFT));
+        }
+    
+        function geraPix($newKeyPix, $idTx = '', $valor = 0.00){
+            $resultado = "000201";
+            $resultado .= formataCampo("26", "0014br.gov.bcb.pix" . formataCampo("01", $newKeyPix));
+            $resultado .= "52040000";
+            $resultado .= "5303986";
+            echo $valor;
+            if($valor > 0){
+                $resultado .= formataCampo("54", number_format($valor, 2, '.', ''));
+            }
+            $resultado .= "5802BR";
+            $resultado .= "5901N";
+            $resultado .= "6001C";
+            $resultado .= formataCampo("62", formataCampo("05", $idTx ?: '***'));
+            $resultado .= "6304";
+            $resultado .= calculaCRC16($resultado);
+            return $resultado;
+        }
+    
+        $codigoPix = geraPix($newKeyPix, $idPix, $brokenValue);
+    
+        $_SESSION['qrcode'] = '<p><img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . urlencode($codigoPix) . '"></p>
+                               <p>Código PIX: '. $codigoPix .'</p>';
+    }
+
+    else if($typeKey == 2){
+
+        function formataCampo($id, $valor){
+            return $id . str_pad(strlen($valor), 2, '0', STR_PAD_LEFT) . $valor;
+        }
+    
+        function calculaCRC16($dados){
+            $resultado = 0xFFFF;
+            for ($i = 0; $i < strlen($dados); $i++){
+                $resultado ^= (ord($dados[$i]) << 8);
+                for ($j = 0; $j <8; $j++){
+                    if($resultado & 0x8000){
+                        $resultado = ($resultado << 1) ^ 0x1021;
+                    } 
+                    else{
+                        $resultado <<= 1;
+                    }
+                    $resultado &= 0xFFFF;
+                }
+            }
+            return strtoupper(str_pad(dechex($resultado), 4, '0', STR_PAD_LEFT));
+        }
+    
+        function geraPix($keyPix, $idTx = '', $valor = 0.00){
+            $resultado = "000201";
+            $resultado .= formataCampo("26", "0014br.gov.bcb.pix" . formataCampo("01", $keyPix));
+            $resultado .= "52040000";
+            $resultado .= "5303986";
+            echo $valor;
+            if($valor > 0){
+                $resultado .= formataCampo("54", number_format($valor, 2, '.', ''));
+            }
+            $resultado .= "5802BR";
+            $resultado .= "5901N";
+            $resultado .= "6001C";
+            $resultado .= formataCampo("62", formataCampo("05", $idTx ?: '***'));
+            $resultado .= "6304";
+            $resultado .= calculaCRC16($resultado);
+            return $resultado;
+        }
+    
+        $codigoPix = geraPix($keyPix, $idPix, $brokenValue);
+    
+        $_SESSION['qrcode'] = '<p><img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . urlencode($codigoPix) . '"></p>
+                               <p>Código PIX: '. $codigoPix .'</p>';
+    }
+    else if($typeKey >= 3){
+
+        $newKeyPix = $broken;
+
+        function formataCampo($id, $valor){
+            return $id . str_pad(strlen($valor), 2, '0', STR_PAD_LEFT) . $valor;
+        }
+    
+        function calculaCRC16($dados){
+            $resultado = 0xFFFF;
+            for ($i = 0; $i < strlen($dados); $i++){
+                $resultado ^= (ord($dados[$i]) << 8);
+                for ($j = 0; $j <8; $j++){
+                    if($resultado & 0x8000){
+                        $resultado = ($resultado << 1) ^ 0x1021;
+                    } 
+                    else{
+                        $resultado <<= 1;
+                    }
+                    $resultado &= 0xFFFF;
+                }
+            }
+            return strtoupper(str_pad(dechex($resultado), 4, '0', STR_PAD_LEFT));
+        }
+    
+        function geraPix($newKeyPix, $idTx = '', $valor = 0.00){
+            $resultado = "000201";
+            $resultado .= formataCampo("26", "0014br.gov.bcb.pix" . formataCampo("01", $newKeyPix));
+            $resultado .= "52040000";
+            $resultado .= "5303986";
+            echo $valor;
+            if($valor > 0){
+                $resultado .= formataCampo("54", number_format($valor, 2, '.', ''));
+            }
+            $resultado .= "5802BR";
+            $resultado .= "5901N";
+            $resultado .= "6001C";
+            $resultado .= formataCampo("62", formataCampo("05", $idTx ?: '***'));
+            $resultado .= "6304";
+            $resultado .= calculaCRC16($resultado);
+            return $resultado;
+        }
+    
+        $codigoPix = geraPix($newKeyPix, $idPix, $brokenValue);
+    
+        $_SESSION['qrcode'] = '<p><img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . urlencode($codigoPix) . '"></p>
+                               <p>Código PIX: '. $codigoPix .'</p>';
+    }
+    header("Location: index.php");
+?>
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <?php session_start(); ?>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Gerador de QR Code PIX</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script type="text/javascript" src="script/script.js"></script>
+    </head>
+    <body>
+        <div class="container">
+                <div class="main">
+                    <form method="post" action="generator.php">
+                        <div class="title"><h3>Gerador QR Code PIX</h3></div>
+                        
+                        <div class="subtitle"><h4>Dados do PIX</h4></div>
+
+                        <div class="keyType">
+                        <label>Tipo da Chave</label><br>
+                            <input type="radio" id="keytype" name="keytype" value="1" onclick="typeMask();" required> Telefone <br>
+                            <input type="radio" id="keytype" name="keytype" value="2" onclick="typeMask();" required> E-mail <br>
+                            <input type="radio" id="keytype" name="keytype" value="3" onclick="typeMask();" required> CPF <br>
+                            <input type="radio" id="keytype" name="keytype" value="4" onclick="typeMask();" required> CNPJ <br>
+                        </div>
+                        <div class="keyTransfer">
+                            <label for="keyTransfer"> Chave Pix</label>
+                            <input type="text" id="keyTransfer" name="keyTransfer" required disabled>
+                        </div>
+                        <div class="nameTransfer">
+                            <label for="nameTransfer"> Nome do Beneficiário (até 25 letras)</label>
+                            <input type="text" id="nameTransfer" name="nameTransfer" maxlength="25" required>
+                        </div>
+                        <div class="cityTransfer">
+                            <label for="cityTransfer"> Cidade (Até 15 letras)</label>
+                            <input type="text" id="cityTransfer" name="cityTransfer" maxlength="15" required>
+                        </div>
+                        <div class="valueTransfer">
+                            <label for="valueTransfer"> Valor (opcional)</label>
+                            <input type="text" id="valueTransfer" name="valueTransfer" oninput="mascaraMoeda(event);" placeholder="R$ 0,00">
+                        </div>
+                        <div class="idTransfer">
+                            <label for="id"> Identificador (opcional)</label>
+                            <input type="text" id="idTransfer" name="idTransfer" placeholder="PAGAMENTO1">
+                        </div>
+                    
+                        <button type="submit"> Gerar Pix </button>
+                    </form>
+                </div>
+
+                <?php
+                if (isset($_SESSION['qrcode'])) {
+                    echo $_SESSION['qrcode'];
+                    unset($_SESSION['qrcode']);
+                }
+                ?>
+        </div>
+    </body>
+</html>
